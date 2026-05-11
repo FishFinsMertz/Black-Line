e44f0d325f904d360acba9eef53521ebfd9fbae1
T-AnBui <113273509+FishFinsMertz@users.noreply.github.com>
2026-05-10 23:00:38 -0400

    Rough Infrared Shader Set up
    Not finished

---
+++ Assets/Scripts/Utility/InfraredToggler.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+// Attach this to a gameobject
+public class InfraredToggler : MonoBehaviour
+{
+    // Toggle the infrared effect on and off with T key
+}
