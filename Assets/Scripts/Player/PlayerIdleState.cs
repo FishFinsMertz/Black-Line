afdf4c579e7c51de522577022ec782537a1272c9
T-AnBui <113273509+FishFinsMertz@users.noreply.github.com>
2026-05-20 22:23:01 -0400

    Hand walk animation added

--- Assets/Scripts/Player/PlayerIdleState.cs
+++ Assets/Scripts/Player/PlayerIdleState.cs
@@ -8,6 +8,7 @@ public class PlayerIdleState : PlayerState
     {
         player.rb.linearVelocity = Vector2.zero;
         player.bodyAnimator.SetFloat("SpeedX", 0.0f);
+        player.SetArmAnimationState(isIdle: true, isWalking: false);
     }
 
     public override void Update()
