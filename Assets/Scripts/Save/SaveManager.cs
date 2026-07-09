b54130e408777952e270e6b5974729277e0678e3
T-AnBui <113273509+FishFinsMertz@users.noreply.github.com>
2026-07-08 20:58:26 -0400

    Renamed keycards, and also fixed key items saving

--- Assets/Scripts/Save/SaveManager.cs
+++ Assets/Scripts/Save/SaveManager.cs
@@ -51,6 +51,12 @@ public class SaveManager : MonoBehaviour
         saveableObjects.RemoveAll(item => item == null);
 
         GameData data = new GameData();
+        
+        if (cachedSaveData != null)
+        {
+            data.collectedAccessItems = new List<string>(cachedSaveData.collectedAccessItems);
+        }
+
         foreach (var saveable in saveableObjects)
             saveable.Save(data);
 
