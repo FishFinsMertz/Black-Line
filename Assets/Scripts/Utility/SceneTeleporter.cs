1017f53936c6873700a5d4a89276f72a181573a7
T-AnBui <113273509+FishFinsMertz@users.noreply.github.com>
2026-07-28 15:56:52 -0400

    Added new test scene

--- Assets/Scripts/Utility/SceneTeleporter.cs
+++ Assets/Scripts/Utility/SceneTeleporter.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class SceneTeleporter : MonoBehaviour
 {
     [SerializeField] private string targetSceneName;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             SaveManager.Instance?.SaveGame();
-            UnityEngine.SceneManagement.SceneManager.LoadScene(targetSceneName);
+            GameSceneManager.Instance.LoadScene(targetSceneName);
         }
     }
 }
